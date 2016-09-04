7b51920999c33195def3e6d0a3c63c45ba03f3e7
Atomos\Mario <mariospapa90@gmail.com>
2016-09-04 16:27:32 +0200

    +Builder editor.(allows you to select a prefab from a list and add it instantly to the scene allowing you to select position rotation and scale)  bug fixed, may need more testing.

---
+++ Assets/Scripts/EditorScripts/Builder.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class Builder : MonoBehaviour {
+
+    private Stack<GameObject> spawnedObjects = new Stack<GameObject>();
+
+    public Vector3 position = Vector3.zero;
+    public Vector3 rotation = new Vector3(0f, 0f, 0f);
+    public Vector3 localScale = new Vector3(1f,1f,1f);
+
+
+    private GameObject[] prefabs;
+
+    private void Start()
+    {
+        prefabs = Resources.LoadAll<GameObject>("Prefabs");
+    }
+
+    public void AddObject(int i)
+    {
+        GameObject tempObj = (GameObject)Instantiate(prefabs[i], position, Quaternion.Euler(rotation));
+        tempObj.transform.localScale = localScale;
+        spawnedObjects.Push(tempObj);
+    }
+
+    public GameObject[] GetGameObjects()
+    {
+        if (prefabs.Length == 0 || prefabs == null)
+        {
+            RefreshListOfObjects();
+        }
+        return prefabs;
+    }
+
+    public void Undo()
+    {
+        if (spawnedObjects.Count > 0)
+        {
+            DestroyImmediate(spawnedObjects.Peek());
+            spawnedObjects.Pop();
+        }
+            
+    }
+
+    public void RefreshListOfObjects()
+    {
+        prefabs = Resources.LoadAll<GameObject>("Prefabs");
+    }
+}
---
+++ Assets/Scripts/EditorScripts/BuilderEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Builder))]
+public class BuilderEditor : Editor {
+
+    private int index = 0;
+
+    public override void OnInspectorGUI()
+    {
+        //so we don't redo the whole inspector but only add to it
+        DrawDefaultInspector();
+
+        Builder builder = (Builder)target;
+        builder.RefreshListOfObjects();
+        string[] options = new string[builder.GetGameObjects().Length];
+
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i] = builder.GetGameObjects()[i].name;
+        }
+
+        index = EditorGUILayout.Popup("Prefabs List:",index, options, EditorStyles.popup);
+
+        if (GUILayout.Button("Add Object"))
+        {
+            builder.AddObject(index);
+        }
+
+        if (GUILayout.Button("Undo"))
+        {
+            builder.Undo();
+        }
+
+    }
+}
