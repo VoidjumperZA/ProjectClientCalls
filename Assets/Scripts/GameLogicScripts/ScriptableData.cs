60711a65c69804f5daa8c4ff2366ef826f2a5bee
Atomos\Mario <mariospapa90@gmail.com>
2016-09-04 22:24:25 +0200

    +ScriptableData files for game logic.

---
+++ Assets/Scripts/GameLogicScripts/ScriptableData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "Difficulty Data", menuName ="Game Logic/Difficulty", order = 1)]
+public class ScriptableData : ScriptableObject {
+    //please put any variables that affect difficulty here first: easy, then medium then hard
+    public string difficulty = "Medium"; //easy,medium/hard
+    public int sanityGain = 0; //example 50,20,0
+}
