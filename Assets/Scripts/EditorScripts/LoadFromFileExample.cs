2d329d08d4b5da80989be51ba9a6ee77ed528950
mariospapa90 <mariospapa90@gmail.com>
2016-09-07 12:04:31 +0200

    +Working example for art replacement. -Hopefully the assetbundlesamples are deleted +Prefabs

---
+++ Assets/Scripts/EditorScripts/LoadFromFileExample.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class LoadFromFileExample : MonoBehaviour
+{
+
+    private void Start()
+    {
+        AssetBundle myLoadedAssetBundle = AssetBundle.LoadFromFile(GetBundlePathForLoadFromFile("mushrooms"));
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        GameObject prefab = myLoadedAssetBundle.LoadAsset<GameObject>("Mushroom");
+        Instantiate(prefab);
+
+        myLoadedAssetBundle.Unload(false);
+    }
+
+    public static string GetBundlePathForLoadFromFile(string relativePath)
+    {
+        string streamingAssetsPath = Application.streamingAssetsPath;
+
+        return Path.Combine(streamingAssetsPath, relativePath);
+    }
+
+}
