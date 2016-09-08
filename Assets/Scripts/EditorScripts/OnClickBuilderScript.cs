7f78fb193a9faec772a5cae2b7fa2ab311283a9c
Atomos\Mario <mariospapa90@gmail.com>
2016-09-08 04:16:37 +0200

    +New scripts to be able to instantiate objects easily

---
+++ Assets/Scripts/EditorScripts/OnClickBuilderScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OnClickBuilderScript : MonoBehaviour {
+
+    private GameObject[] prefabs;
+    private Dropdown dropdown;
+
+    private void Start()
+    {
+        prefabs = Resources.LoadAll<GameObject>("Prefabs");
+        dropdown = FindObjectOfType<Dropdown>();
+        dropdown.options.Clear();
+
+        for (int i = 0; i < prefabs.Length; i++)
+        {
+            dropdown.options.Add(new Dropdown.OptionData(prefabs[i].name));
+        }
+        dropdown.captionText.text = prefabs[dropdown.value].name;
+    }
+
+    private void Update()
+    {
+        Build();
+    }
+
+    private void Build()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)), out hitInfo))
+            {
+                Instantiate(prefabs[dropdown.value], hitInfo.point, Quaternion.identity);
+            }
+        }
+    }
+}
