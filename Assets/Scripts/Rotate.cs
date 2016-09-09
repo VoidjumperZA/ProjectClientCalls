a89062d4bea8375e31eb7cf0bb141900a67f57c1
Josh van Asten (ACER 940M) <joshmathew.va@gmail.com>
2016-09-09 17:53:18 +0200

    o Training scene is now colour-coded for segments that 'belong' together

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour
+{
+
+    [SerializeField]
+    private float xRotation;
+
+    [SerializeField]
+    private float yRotation;
+
+    [SerializeField]
+    private float zRotation;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xRotation, yRotation, zRotation);
+    }
+}
